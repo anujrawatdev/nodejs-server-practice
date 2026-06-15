2ec0e9aa9c7cd9a351b6097e7a62fa4d11381bad
Anuj Rawat <rawatanuj532@gmail.com>
2026-06-15 16:08:23 +0530

    Implement URL routing, query parsing, and request logging

--- index.js
+++ index.js
@@ -1,18 +1,34 @@
 const http = require("http");
 const fs = require("fs");
-const myServer = http.createServer((req,res)=>{
-    const log = `${Date.now()}: ${req.url} New req received\n`;
-    fs.appendFile('log.txt', log , (err,data)=>{
+const url = require("url"); 
 
-        switch(req.url){
+const myServer = http.createServer((req,res)=>{
+
+    if(req.url==='/favicon.ico') return res.end();
+
+    const log = `${Date.now()}: ${req.url} New req received\n`;
+    const myUrl = url.parse(req.url , true);
+    console.log(myUrl);
+
+    fs.appendFile('log.txt', log , (err,data)=>{
+        
+        switch(myUrl.pathname){
             case '/':res.end("Homepage");
             break;
-            case '/about':res.end("This is About page");
+            case '/about':
+
+            const username = myUrl.query.myname;    
+            res.end(`Hi,${username}`);
             break;
+            
+            case '/search':
+            const search = myUrl.query.search_query;
+            res.end('here are your result for '+ search);
+
             default : 
             res.end("404 not found");
         }
-
+  
     });
 
 });
